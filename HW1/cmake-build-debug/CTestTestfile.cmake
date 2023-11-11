54bba57492996d93173bdd988913b4c46729b651
Хроменок Роман Владимирович <rkhromenok@t-argos.ru>
2023-11-11 13:18:33 +0200

    init2

--- HW1/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/OTUS-C-Professional/HW1
-# Build directory: /tmp/OTUS-C-Professional/HW1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
-set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/tmp/OTUS-C-Professional/HW1/CMakeLists.txt;74;add_test;/tmp/OTUS-C-Professional/HW1/CMakeLists.txt;0;")
